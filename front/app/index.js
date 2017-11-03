6cee12ddc0042629a2e1e9ead310d8d614fd4bce
Nicolas P <nicolas@n-p.fr>
2017-11-03 12:29:41 +0100

    Added basic theme and home behavior

--- front/app/index.js
+++ front/app/index.js
@@ -1,8 +1,45 @@
 angular.module('PrivacyInformant', ['ngMaterial'])
 .config(function($mdThemingProvider) {
+
+  $mdThemingProvider.definePalette('pipalette', {
+    '50': 'e6f8ed',
+    '100': 'aae8c2',
+    '200': '7edba3',
+    '300': '46cc7b',
+    '400': '35bf6b',
+    '500': '2ea75e',
+    '600': '278f50',
+    '700': '217743',
+    '800': '1a5f35',
+    '900': '144728',
+    'A100': 'd8fce6',
+    'A200': '78f5aa',
+    'A400': '2cdc72',
+    'A700': '2ec168',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': [
+      '50',
+      '100',
+      '200',
+      '300',
+      '400',
+      'A100',
+      'A200',
+      'A400',
+      'A700'
+    ],
+    'contrastLightColors': [
+      '500',
+      '600',
+      '700',
+      '800',
+      '900'
+    ]
+  });
+
   $mdThemingProvider.theme('default')
-    .primaryPalette('blue')
-    .accentPalette('pink');
+    .primaryPalette('pipalette')
+    .accentPalette('blue');
 })
 .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
    $scope.toggleLeft = buildDelayedToggler('left');
@@ -50,8 +87,8 @@ angular.module('PrivacyInformant', ['ngMaterial'])
    };
 
    $scope.menuContent = [{
-     title: "Make a request",
-     path: "/request"
+     title: "Follow a request",
+     path: "/follow"
    }, {
      title: "Services list",
      path: "/services"
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('front'));
+app.use(express.static('front/app'));
 
 var routes = require('./api/routes/privacyInformantRoutes'); //importing route
 
